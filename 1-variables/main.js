37c50bf031ea037c9d69ebe9ff96758704e8c68e
babooka <metallocalypse@gmail.com>
2021-09-23 18:24:10 +0300

    first homework

---
+++ 1-variables/main.js
@@ -0,0 +1,40 @@
+const item1 = 5.678, item2 = 123.965, item3 = 90.2345;
+
+let meat = item2;
+let bread = item1;
+let cheese = item3;
+let sumRound = (Math.floor(meat) + Math.floor(cheese) + Math.floor(bread) );
+let sum = (meat + bread + cheese);
+let sale = Math.random();
+
+console.log ('Meat price is ' + meat + '\nBread price is ' + bread + '\nCheese price is ' + cheese);
+
+console.log("Max price is " + Math.max(meat, bread, cheese) + "\nMin price is " + Math.min(meat, bread, cheese));
+
+console.log("Summ is " + sum);
+
+console.log("Summ floor is " + (Math.floor(meat) + Math.floor(cheese) + Math.floor(bread) ) );
+
+console.log("Summ round is " + Math.round(sumRound/100)*100);
+
+
+
+
+function isEven () {
+    if ((Math.floor(sum)) % 2 == 0) {
+       return true }
+            else {
+        return  false }
+    };
+
+
+console.log("Summ Even is " + (isEven()));
+
+console.log('Change from 500 is ' + (500 - sum));
+
+console.log('Price average is ' + Math.round((sum/3) * 100)/100);
+
+
+console.log('Discount price is ' + (Math.round((sum*sale)*100)/100));
+
+console.log('Original price is ' + sum + '\nDiscount is ' + (Math.round(sale*100)) + '%' + '\nPayed ' + (sum-(sum*sale)) + '\nCost price is ' + (sum/2) + '\nProfit ' + ((sum/2)-(sum-(sum*sale))));
