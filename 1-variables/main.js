ac46cdb17574f52a13a2c94bec985f947e31e5f2
babooka <metallocalypse@gmail.com>
2021-09-29 18:24:07 +0300

    fixed formulas and variables

--- 1-variables/main.js
+++ 1-variables/main.js
@@ -1,9 +1,7 @@
 const meat = 15.678, bread = 123.965, cheese = 90.2345;
-
-const sumRound = Math.round(meat) + Math.round(cheese) + Math.round(bread);
 const sum = meat + bread + cheese;
-let sale = Math.random();
-let discountPrice = sum*sale;
+const sale = Math.random();
+const discountPrice = sum*sale;
 
 console.log ('Meat price is ' + meat + '\nBread price is ' + bread + '\nCheese price is ' + cheese);
 
@@ -13,7 +11,7 @@ console.log("Summ is " + sum);
 
 console.log("Summ floor is " + (Math.floor(meat) + Math.floor(cheese) + Math.floor(bread) ) );
 
-console.log("Summ round is " + (Math.floor((sum)/100)) * 100);
+console.log("Summ round is " + (Math.round((sum)/100)) * 100);
 
 function isEven () {
     if ((Math.floor(sum)) % 2 === 0) {
@@ -22,12 +20,12 @@ function isEven () {
         return  false }
     };
 
-console.log("Summ Even is " + (isEven()));
+console.log("Summ even is " + isEven() );
 
 console.log('Change from 500 is ' + (500 - sum));
 
 console.log('Average price ' + (sum/3).toFixed(2));
 
-console.log('Discount price is ' + (Math.round(discountPrice*100)/100));
+console.log('Discount price is ' + discountPrice.toFixed(2));
 
-console.log('Original price is ' + sum + '\nDiscount is ' + (Math.round(sale*100)) + '%' + '\nPayed ' + (sum-discountPrice) + '\nCost price is ' + (sum/2) + '\nProfit ' + ((sum/2)-(sum-discountPrice)));
+console.log('Original price is ' + sum + '\nDiscount is ' + (Math.round(sale*100)) + '%' + '\nPayed ' + (sum-discountPrice).toFixed(2) + '\nCost price is ' + (sum/2) + '\nProfit ' + ((sum/2)-discountPrice).toFixed(2));
